cc4267c6289b39421d9dc3dd7783ed7b10f162a1
Palchhin <palchhinparihar@gmail.com>
2026-01-11 21:20:59 +0530

    fix: reorder and update clients list for better organization

--- frontend/src/data/clients.js
+++ frontend/src/data/clients.js
@@ -6,8 +6,15 @@ export const clients = [
   "POSCO",
   "Embassy of Korea",
   "Boryung Pharma",
-  "Grace Pharma",
-  "PolyPeptide Group",
+  "Myung In Pharma Korea",
+  "Leader's Bio Korea",
+  "ALS Pharma Korea",
+  "Ace Pharma Korea",
+  "Whawon Pharma Korea",
+  "Gmax Pharmachem Korea",
+  "BnP Care Korea",
+  "Vivpharms Korea",
+  "Samoh Pharma Korea",
   "S.Y. System Korea",
 
   // Indian Pharmaceutical Companies
@@ -20,6 +27,12 @@ export const clients = [
   "Rubicon Research",
   "Bluestar India",
   "Nosch Labs",
+  "Synthokem Labs",
+  "Amoli Organics",
+  "Sonal Organics",
+  "Grace Pharma",
+  "PolyPeptide Group",
+  "Repute Health Care Korea",
 
   // Finance & Others
   "NH Capital",
